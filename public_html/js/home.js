42f7f4c9917226dff0d45f131e6635d5972d5ae4
ChrisOgal <ChrisOgal@users.noreply.github.com>
2020-12-29 16:16:45 -0500

    logic fully functional

--- public_html/js/home.js
+++ public_html/js/home.js
@@ -10,46 +10,92 @@ function getWeather() {
 }
 
 function loadFiveDays() {
-  var fiveDayRows = $("#currentTableItem");
+  var fiveDayRows = $("#fiveDayItem");
   $.ajax({
     type: "GET",
     url:
       "http://api.openweathermap.org/data/2.5/forecast?zip=" +
       $("#zipcode").val() +
-      "units=" +
-      $("#unitSelect").val() +
-      "&appid=####",
+      "&units=" +
+      $("#unitSelect").find(":selected").text().toLowerCase() +
+      "&appid=4868dfff7b4fc320326ab9b41baec745",
 
     headers: {
       Accept: "application/json",
     },
-    success: function (fiveDayForecast) {
-      $.each(fiveDayForecast, function (index, forecast) {
-        var weatherDate = new Date(forecast.list.dt);
+    success: function (forecastArray) {
+      const fiveDayArray = [];
+
+      forecastArray.list.forEach(function (item) {
+        var sec = item.dt;
+        var dummyTime = new Date(sec * 1000);
+        var hour = dummyTime.getHours();
+        var minutes = dummyTime.getMinutes();
+
+        if (hour == 13 && minutes == 0) {
+          fiveDayArray.push(item);
+        }
+      });
+
+      console.log(fiveDayArray);
+
+      var month = new Array();
+      month[0] = "Jan";
+      month[1] = "Feb";
+      month[2] = "Mar";
+      month[3] = "Apr";
+      month[4] = "May";
+      month[5] = "Jun";
+      month[6] = "Jul";
+      month[7] = "Aug";
+      month[8] = "Sep";
+      month[9] = "Oct";
+      month[10] = "Nov";
+      month[11] = "Dec";
+      $.each(fiveDayArray, function (index, forecast) {
+        var weatherDate = new Date(forecast.dt * 1000);
         var imgUrl =
           "http://openweathermap.org/img/w/" +
-          forecast.list.weather.icon +
+          forecast.weather[0].icon +
           ".png";
-        var mainCondition = forecast.list.weather.main;
-        var highTemp = forecast.list.temp_max;
-        var lowTemp = forecast.list.temp_min;
+        console.log(imgUrl);
+        var mainCondition = forecast.weather[0].main;
+        var highTemp = forecast.main.temp_max;
+        var lowTemp = forecast.main.temp_min;
+        var fOrC =
+          $("#unitSelect").find(":selected").text() == "Imperial" ? "F" : "C";
 
-        var dateRow = "<tr> <td>" + weatherDate + "</tr>";
+        var dateRow =
+          "<tr> <td>" +
+          weatherDate.getDate() +
+          " " +
+          month[weatherDate.getMonth()] +
+          "</tr>";
         var conditionsRow =
-          "<tr><td><img src=" +
-          imgUrl +
-          'alt="weather image">' +
-          mainCondition +
-          "</td></tr>";
+          "<tr><td><img src=" + imgUrl + ">" + mainCondition + "</td></tr>";
         var tempsRow =
-          "<tr><td>H " + highTemp + " C L" + lowTemp + " C</td></tr>";
+          "<tr><td>H " +
+          highTemp +
+          " " +
+          fOrC +
+          " L" +
+          lowTemp +
+          " " +
+          fOrC +
+          "</td></tr>";
 
         fiveDayRows.append(dateRow);
         fiveDayRows.append(conditionsRow);
         fiveDayRows.append(tempsRow);
       });
     },
-    error: function () {},
+    error: function () {
+      $("#errorMessages").append(
+        $("<li>")
+          .attr({ class: "list-group-item list-group-item-danger" })
+          .text("Error calling web service. Please try again later.")
+      );
+    },
   });
 }
 function loadCurrent() {
@@ -63,7 +109,7 @@ function loadCurrent() {
     url:
       "http://api.openweathermap.org/data/2.5/weather?zip=" +
       $("#zipcode").val() +
-      "&appid=b7ee0197c4cee473050440d520c120c0" +
+      "&appid=4868dfff7b4fc320326ab9b41baec745" +
       "&units=" +
       $("#unitSelect").find(":selected").text().toLowerCase(),
     headers: {
