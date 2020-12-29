01aec1e22239439cd236e13df6f82226d15e94bc
ChrisOgal <ChrisOgal@users.noreply.github.com>
2020-12-29 12:35:46 -0500

    initial set up

--- public_html/js/home.js
+++ public_html/js/home.js
@@ -1,39 +1,52 @@
-$(document).ready(function(){
-   getWeather(); 
+$(document).ready(function () {
+  getWeather();
 });
 
-
-function getWeather(){
-    $('#getWeatherButton').click(function(event){
-        loadCurrent();
-        loadFiveDays();
-        
-        
-    });
+function getWeather() {
+  $("#getWeatherButton").click(function (event) {
+    loadCurrent();
+    loadFiveDays();
+  });
 }
 
-
-function loadCurrent(){
-    $.ajax({
-    type: 'GET',
-    url: 'http://api.openweathermap.org/data/2.5/weather?zip=' + $('#zipcode').val() +
-            '&appid=###',
-    headers:{
-        'Accept' : 'application/json'
+function loadCurrent() {
+  $.ajax({
+    type: "GET",
+    url:
+      "http://api.openweathermap.org/data/2.5/weather?zip=" +
+      $("#zipcode").val() +
+      "&appid=###",
+    headers: {
+      Accept: "application/json",
     },
-    success: function(){
-
-        
-    },
-    error: function(){
-        
-    }
-    
-}) ;
-        
+    success: function () {},
+    error: function () {},
+  });
 }
 
-function loadFiveDays(){
-    
-    
+function loadFiveDays() {
+  $.ajax({
+    type: "GET",
+    url:
+      "http://api.openweathermap.org/data/2.5/forecast?zip=" +
+      $("#zipcode").val() + 'units=' + $().val() +  
+      "&appid=####",
+
+    headers: {
+      Accept: "application/json",
+    },
+    success: function (fiveDayForecast) {
+      $.each(fiveDayForecast, function (index, forecast) {
+        var weatherDate = new Date(forecast.list.dt);
+        var imgUrl =
+          "http://openweathermap.org/img/w/" +
+          forecast.list.weather.icon +
+          ".png";
+        var mainCondition = forecast.list.weather.main;
+        var highTemp = ;
+        var lowTemp = ;
+      });
+    },
+    error: function () {},
+  });
 }
